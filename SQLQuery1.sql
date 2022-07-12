e6ed43bdce3cfe86ad3b0e075a6ca00ae78aa8f1
URmyArtemis <zhenhangji@gmail.com>
2022-07-12 09:52:52 -0400

    first attempt

---
+++ SQLQuery1.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Quote] (
+    [QuoteID] INT IDENTITY (1, 1) NOT NULL,
+    [Quote Type] NVARCHAR (200) NULL,
+    [Contact]  NVARCHAR (200) NULL,
+    [Task] NVARCHAR (200) NULL,
+    [Due Date] DATE NULL,
+    [Task Type] NVARCHAR (200) NULL,
+    CONSTRAINT [PK_dbo.Quote] PRIMARY KEY CLUSTERED ([QuoteID] ASC)
+);
+
